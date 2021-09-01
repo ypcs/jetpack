d077377917c4346d0bcc85161c098f822282417f
jboland88 <18016357+jboland88@users.noreply.github.com>
2021-09-01 08:49:10 -0400

    Initial commit to introduce an updated disconnection flow

--- projects/plugins/jetpack/tools/webpack.config.js
+++ projects/plugins/jetpack/tools/webpack.config.js
@@ -44,6 +44,7 @@ module.exports = [
 		entry: {
 			admin: path.join( path.dirname( __dirname ), '_inc/client', 'admin.js' ),
 			'search-dashboard': path.join( __dirname, '../_inc/client', 'search-dashboard-entry.js' ),
+			'plugin-deactivate': path.join( __dirname, '../_inc/client', 'plugin-deactivate-entry.js' ),
 		},
 		plugins: [
 			...sharedWebpackConfig.plugins,
